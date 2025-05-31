03a192598dde8f382998c7c8b89519132be4f3ca
KornchawanCS801 <Kornchawan.pasi@mail.kmutt.ac.th>
2025-05-31 11:57:40 +0700

    cookie

--- Backend/src/controllers/user.controller.ts
+++ Backend/src/controllers/user.controller.ts
@@ -1,5 +1,7 @@
 import type { Context } from "hono";
 import * as userModel from "../models/user.model.ts";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt";
 
 type createUserBody = {
 	username: string;
@@ -10,6 +12,7 @@ type createUserBody = {
 const createUser = async (c: Context) => {
 	try {
 		const body = await c.req.json<createUserBody>();
+		const hashedPassword = await bcrypt.hash(body.password, 10);
 		if (!body.username || !body.email || !body.password)
 			return c.json(
 				{
@@ -29,7 +32,7 @@ const createUser = async (c: Context) => {
 		const newUser = await userModel.createUser(
 			body.username,
             body.email,
-            body.password
+            hashedPassword
 		);
 		return c.json({
 			success: true,
@@ -51,7 +54,12 @@ const createUser = async (c: Context) => {
 const getAllInfoUser = async (c: Context) => {
      try {
  		const user = await userModel.getAllInfoUser();
-         return c.json(user);
+         return c.json({
+  			success: true,
+  			data: user,
+	});
+
+		 
  	} catch (e) {
  		return c.json(
  			{
@@ -87,17 +95,39 @@ const getAllInfoUser = async (c: Context) => {
 }
 
 const loginUser = async (c: Context) => {
-	try {
-		const { email,password } = await c.req.json();
-		const user = await userModel.loginUser(email, password);;
-		return c.json({
-			success: true,
-			data: user,
-			msg: "Already login",
+  try {
+    const { email, password } = await c.req.json();
+    const user = await userModel.loginUser(email);
+
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return c.json({ success: false, msg: "Invalid credentials" }, 401);
+    }
+
+    const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET!, {
+      expiresIn: "1d",
+    });
+
+    c.header("Set-Cookie", `token=${token}; HttpOnly; Path=/; Max-Age=604800`);
+    c.set("user", user);
+
+    return c.json({ 
+		success: true,
+		 msg: "Logged in",
+		 data: { id: user.id, username: user.username } 
 		});
-	} catch (e) {
-		return c.json({ success: false, data: null, msg: `${e}` }, 500);
-	}
+  } catch (e) {
+    return c.json({ success: false, msg: `${e}` }, 500);
+  }
+};
+
+const getMe = async (c: Context) => {
+  const user = c.get("user"); // มาจาก middleware
+  return c.json({ success: true, data: user });
+};
+
+const logOut = (c: Context) => {
+  c.header("Set-Cookie", `token=; HttpOnly; Path=/; Max-Age=0`);
+  return c.json({ success: true, msg: "Logged out" });
 };
 
 const editUsername = async (c: Context) => {
@@ -124,4 +154,4 @@ const editUsername = async (c: Context) => {
 	}
 }
 
-export { createUser, getInfoUser, loginUser, getAllInfoUser, editUsername }
+export { createUser, getInfoUser, loginUser, getAllInfoUser, editUsername,getMe,logOut }
