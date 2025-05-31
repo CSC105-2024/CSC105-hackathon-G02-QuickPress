8c5fc55fd18e54cb2ee7ca174e6359076c065a46
Sittipu <stpppr2548@gmail.com>
2025-05-31 10:22:30 +0700

    MainPage

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+// Pages
+import MainPage from './pages/MainPage.jsx';
 
 function App() {
-
   return (
     <>
-      
+      <MainPage />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
