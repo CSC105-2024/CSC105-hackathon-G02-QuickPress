c270dc37df39f1f0abf8be23944abb9868403880
GetMaEng <kawin.ka.get@gmailcom>
2025-05-31 08:52:03 +0700

    Set up

---
+++ Frontend/src/main.jsx
@@ -0,0 +1,18 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom"; //import these modules
+import "./index.css";
+import App from "./App.jsx";
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+
+const router = createBrowserRouter([
+  {
+    path: "/", // Home route
+    element: <App />, // Render the App component
+  },
+]);
